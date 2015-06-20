2e1944d1892e20cffeb342f319e94ddcd6868656
HaleyBowler <hjb2dq@virginia.edu>
2015-06-20 15:25:09 +0200

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+
+  acts_as_messageable
+
+def mailboxer_email(object)
+  #Check if an email should be sent for that object
+  #if true
+  return "define_email@on_your.model"
+  #if false
+  #return nil
+end
+  	
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
