3b6497740d575e657e6ba9bf2a06a6e4be5f1934
RachelKnapp <rachelkknapp@gmail.com>
2015-06-22 20:36:16 +0200

    changes to the layout configuration

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150622120553) do
 
   create_table "languages", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
