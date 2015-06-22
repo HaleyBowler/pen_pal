c8bc943dd11cbfefa047e24d87b389666ddddeac
HaleyBowler <hjb2dq@virginia.edu>
2015-06-22 21:38:43 +0200

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622120553) do
+ActiveRecord::Schema.define(version: 20150622191751) do
 
   create_table "languages", force: :cascade do |t|
     t.string   "name"
@@ -95,6 +95,11 @@ ActiveRecord::Schema.define(version: 20150622120553) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "location"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
