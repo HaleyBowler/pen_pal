01f60dcb6740396e8907daa23c311b3e78ae3779
HaleyBowler <hjb2dq@virginia.edu>
2015-06-22 16:23:44 +0200

    No longer going down horrible path of destruction

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :conversations, only: [:index, :show, :destroy]
   resources :messages, only: [:new, :create]
-
+  # resources :languages, only: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
