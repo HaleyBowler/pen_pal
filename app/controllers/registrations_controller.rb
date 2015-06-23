78d9be181bda2aaa03bfe47b3cc993bc1519e205
RachelKnapp <rachelkknapp@gmail.com>
2015-06-23 16:32:41 +0200

    changed the toolbar again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  
-  def after_sign_in_path_for(users)
-    redirect_to users_path
-  end
-
   before_filter :configure_permitted_parameters
   protected
 
