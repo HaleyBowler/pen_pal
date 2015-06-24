03226c20fbcb40fd77733b39d007a2297fbc74d7
jasonbhaas <jasonbhaas@gmail.com>
2015-06-24 11:09:51 +0200

    don't know what exactly i'm going, jut commiting in order to pul

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,11 +7,12 @@ class RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :last_name, 
-        :email, :password, :password_confirmation, :location, :avatar)
+        :email, :password, :password_confirmation, :location, 
+        :avatar)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:first_name, :last_name, :current_password,
-        :email, :password, :password_confirmation, :location, :avatar)
+        :email, :location, :avatar)
     end
   end
 end
